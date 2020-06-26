92fa16730df1f7d69556752dcfc024074292ff2a
SahilKr24 <smart.is.sahil.kumar@gmail.com>
2020-06-26 19:43:05 +0000

    added price comp. and fav button

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,211 +1,264 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sale_spree/components/item.dart';
 
 class DetailScreen extends StatefulWidget {
   final imagePath;
   final herotag;
   final name;
+  Item item;
 
-  DetailScreen(this.imagePath,this.herotag,this.name);
+  DetailScreen(this.imagePath,this.herotag,this.name,{this.item});
 
   @override
   _DetailScreenState createState() => _DetailScreenState();
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  Icon like;
+  bool liked;
+
+  void swapIcon(){
+    like = liked ? Icon(Icons.favorite,color: Colors.red,) : Icon(
+      Icons.favorite_border,
+      size: 30,
+    );
+  }
+
+  @override
+  void initState() {
+    liked = false;
+    like = Icon(
+      Icons.favorite_border,
+      size: 30,
+    );
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              child: Stack(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.height * 0.5 + 16,
-                        bottom: 20,
-                        right: 32,
-                        left: 32),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(60),
-                          bottomRight: Radius.circular(60),
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.5 + 16,
+                          bottom: 20,
+                          right: 32,
+                          left: 32),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(60),
+                            bottomRight: Radius.circular(60),
+                          ),
+                          color: Colors.white),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.item.item,
+                            style: GoogleFonts.poppins(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w800),
+                          ),
+
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 80),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.message,
+                                      size: 22,
+                                    ),
+                                    SizedBox(
+                                      width: 4,
+                                    ),
+                                    Text(
+                                      widget.item.desc,
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+
+                    Container(
+                      child: Hero(
+                        tag: widget.herotag,
+                        child: Container(
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.height * 0.5,
+                          child: ClipRRect(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    widget.item.imageUrl
+                                  ),
+                                  fit: BoxFit.cover
+                                )
+                              ),
+                            ),
+                            borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(60),
+                              bottomLeft: Radius.circular(60),
+                            ),
+                          ),
                         ),
-                        color: Colors.white),
-                    child: Column(
+                      ),
+                    ),
+
+                    Positioned(
+                      top: 40,
+                      left: 24,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+
+                    Positioned(
+                      child: RawMaterialButton(
+                        fillColor: Colors.white,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
+                        elevation: 1,
+                        highlightElevation: 1,
+                        constraints: BoxConstraints.tightFor(
+                          width: 60.0,
+                          height: 60.0,
+                        ),
+                        onPressed: (){
+                          setState(() {
+                            liked = !liked;
+                            swapIcon();
+                          });
+                        },
+                        padding: const EdgeInsets.all(16),
+                        child: AnimatedContainer(
+                          curve: Curves.bounceInOut,
+                          duration: Duration(seconds: 5),
+                          child: like
+                        ),
+                      ),
+                      right: 34,
+                      bottom: 80,
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: 24,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Text(
+                  "Prices across sites",
+                  style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+
+              SizedBox(
+                height: 16,
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  children: [
+                    priceElement(context,assetName: "assets/amazon.svg",price: widget.item.price),
+                    priceElement(context,assetName: "assets/flipkart.svg",price: widget.item.flipkart),
+                    priceElement(context,assetName: "assets/snapdeal.svg",price: widget.item.snapdeal)
+                  ],
+                )
+              ),
+
+              Spacer(),
+
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60),
+                    ),
+                    color: Colors.white),
+                child: Row(
+                  children: [
+                    Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          widget.name,
+                          "Footer Notes",
                           style: GoogleFonts.poppins(
-                              fontSize: 24,
-                              fontWeight: FontWeight.w800),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600),
                         ),
-
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(right: 80),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.person,
-                                    size: 22,
-                                  ),
-                                  SizedBox(
-                                    width: 4,
-                                  ),
-                                  Text(
-                                    "Offered By: IoT Lab",
-                                    style: GoogleFonts.poppins(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
+                        Text(
+                          "Some Footer Text(if any)",
+                          style: GoogleFonts.poppins(
+                              fontSize: MediaQuery.of(context).size.width * 0.05,
+                              fontWeight: FontWeight.w700),
                         )
                       ],
                     ),
-                  ),
 
-                  Container(
-                    child: Hero(
-                      tag: widget.herotag,
-                      child: Container(
-                        width: double.infinity,
-                        height: MediaQuery.of(context).size.height * 0.5,
-                        child: ClipRRect(
-                          child: SvgPicture.asset(
-                            widget.imagePath,
-                            fit: BoxFit.cover,
-                          ),
-                          borderRadius: BorderRadius.only(
-                            bottomRight: Radius.circular(60),
-                            bottomLeft: Radius.circular(60),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  Positioned(
-                    top: 40,
-                    left: 24,
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.arrow_back_ios,
-                        color: Colors.white,
-                        size: 30,
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ),
-
-                  Positioned(
-                    child: RawMaterialButton(
-                      fillColor: Colors.white,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
-                      elevation: 1,
-                      highlightElevation: 1,
-                      constraints: BoxConstraints.tightFor(
-                        width: 60.0,
-                        height: 60.0,
-                      ),
-                      onPressed: (){},
-                      padding: const EdgeInsets.all(16),
-                      child: Icon(
-                        Icons.message,
-                        size: 30,
-                      ),
-                    ),
-                    right: 34,
-                    bottom: 80,
-                  ),
-                ],
-              ),
-            ),
-
-            SizedBox(
-              height: 24,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Text(
-                "Service Description",
-                style: GoogleFonts.poppins(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600),
-              ),
-            ),
-
-            SizedBox(
-              height: 16,
-            ),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-                style: GoogleFonts.poppins(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500),
-              ),
-            ),
-
-            Spacer(),
-
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 20),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(60),
-                    topRight: Radius.circular(60),
-                  ),
-                  color: Colors.white),
-              child: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Footer Notes",
-                        style: GoogleFonts.poppins(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      Text(
-                        "Some Footer Text(if any)",
-                        style: GoogleFonts.poppins(
-                            fontSize: MediaQuery.of(context).size.width * 0.05,
-                            fontWeight: FontWeight.w700),
-                      )
-                    ],
-                  ),
-
-                  Spacer(),
-                ],
-              ),
-            )
-          ],
+                    Spacer(),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Row priceElement(BuildContext context,{String assetName,double price}) {
+    var size = MediaQuery.of(context).size;
+    return Row(
+                  children: [
+                    Container(child: SvgPicture.asset(assetName,width: 65,)),
+                    SizedBox(width: 20,),
+                    Center(child: Text("₹" + price.toString(),textScaleFactor: 1.5,textAlign: TextAlign.right,style: GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w600),))
+                  ],
+                );
   }
 }
